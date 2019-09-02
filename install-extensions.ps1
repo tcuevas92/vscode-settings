a5378f40609a3f888ecc24e40fa87b4386223cc3
TJ Cuevas <tcuevas92@gmail.com>
2019-09-02 13:07:03 -0400

    Initial commit

---
+++ install-extensions.ps1
@@ -0,0 +1,6 @@
+code --install-extension ms-vscode.vs-keybindings
+code --install-extension 2gua.rainbow-brackets
+code --install-extension ms-vscode.powershell
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension mycelo.embrace
+code --install-extension britesnow.vscode-toggle-quotes
