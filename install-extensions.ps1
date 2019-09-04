33a3ba3eedf869e6d0091a32bc22f3d15f75a388
TJ Cuevas <tcuevas92@gmail.com>
2019-09-04 19:26:22 -0400

    Use bracket pair colorizer instead of rainbow brackets

--- install-extensions.ps1
+++ install-extensions.ps1
@@ -1,5 +1,5 @@
 code --install-extension ms-vscode.vs-keybindings
-code --install-extension 2gua.rainbow-brackets
+code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension ms-vscode.powershell
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension mycelo.embrace
